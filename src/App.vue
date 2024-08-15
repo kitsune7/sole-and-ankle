9c613e3bf4a0eb94e71ec40be72581b7e290c67a
Christopher Bradshaw <chris.kofox@gmail.com>
2024-08-15 16:47:27 -0600

    change to Vue

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { Head } from '@unhead/vue/components'
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <Head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta name="description" content="Web site created using Vue3" />
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Raleway:wght@500;600;800&display=swap"
+      rel="stylesheet"
+    />
+
+    <title>Sole&Ankle — CSS for JavaScript Developers</title>
+  </Head>
+
+  <Header />
+  <main></main>
+</template>
+
+<style scoped>
+main {
+  padding: 64px 32px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <header>
+    <div class="super-header">
+      <span class="marketing-message">Free shipping on domestic orders over $75!</span>
+    </div>
+    <div class="main-header">
+      <nav>
+        <a href="/sale">Sale</a>
+        <a href="/new">New&nbsp;Releases</a>
+        <a href="/men">Men</a>
+        <a href="/women">Women</a>
+        <a href="/kids">Kids</a>
+        <a href="/collections">Collections</a>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.super-header {
+  font-size: 0.875rem;
+  color: var(--color-gray-300);
+  background-color: var(--color-gray-900);
+  padding: 10px;
+  text-align: center;
+}
+.marketing-message {
+  color: white;
+}
+
+.main-header {
+  padding: 0 32px;
+  border-bottom: 1px solid var(--color-gray-300);
+}
+nav a {
+  font-size: 1.125rem;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: var(--color-gray-900);
+  font-weight: var(--weight-medium);
+
+  &:first-of-type {
+    color: var(--color-secondary);
+  }
+}
+</style>
