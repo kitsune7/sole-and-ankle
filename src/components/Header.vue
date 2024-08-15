3c33e538c7db518fcaf8bcbb809a64e24cee39ff
Christopher Bradshaw <chris.kofox@gmail.com>
2024-08-15 17:06:31 -0600

    align items in marketing header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,16 @@
+<script setup lang="ts">
+import Cart from './Cart.vue'
+</script>
+
 <template>
   <header>
     <div class="super-header">
       <span class="marketing-message">Free shipping on domestic orders over $75!</span>
+      <div class="right-section">
+        <input type="search" placeholder="Search..." />
+        <a href="/help">Help</a>
+        <a href="/cart"><Cart /></a>
+      </div>
     </div>
     <div class="main-header">
       <nav>
@@ -18,14 +27,38 @@
 
 <style scoped>
 .super-header {
-  font-size: 0.875rem;
-  color: var(--color-gray-300);
+  align-items: center;
   background-color: var(--color-gray-900);
-  padding: 10px;
-  text-align: center;
+  color: var(--color-gray-300);
+  display: flex;
+  font-size: 0.875rem;
+  justify-content: space-between;
+  padding: 8px 32px;
+}
+.super-header a {
+  color: var(--color-gray-300);
+  text-decoration: none;
+}
+.super-header a:hover {
+  color: var(--color-white);
+  text-decoration: underline;
 }
 .marketing-message {
-  color: white;
+  color: var(--color-white);
+}
+.right-section {
+  display: flex;
+  align-items: center;
+  gap: 16px;
+}
+input[type='search'] {
+  background-color: transparent;
+  border: 0;
+  border-bottom: 1px solid var(--color-gray-300);
+  color: var(--color-white);
+}
+input[type='search']::placeholder {
+  color: var(--color-gray-300);
 }
 
 .main-header {
